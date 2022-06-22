38dba070fcd41c94949c2dc26a02badbf3c977de
b4stYgp <82322390+b4stYgp@users.noreply.github.com>
2022-06-22 16:46:45 +0200

    Update index.php
    opens submit.php in new tab

--- index.php
+++ index.php
@@ -1,10 +1,17 @@
-
 <html>
 	<body>
 		<h2>Webcrawler</h2>
-		<form action="/submit.php" method="post">
+		<form target="_blank" action="/submit.php" method="post" id="crawlerForm">
 			<label>DHBW add URL to Database</label>
-			<input type="text" name="url"><br><br>			
+			<input type="text" name="url"><br><br>
+			<p>KEIN https:// -- nur www.xyz.de</p>
 		</form>
-	</body>
+		<button type="submit" form="crawlerForm" value="Submit">Submit</button>
+	
+	<br>
+	<br>
+	<br>
+	<hr>
+	<br>	
+	</body>	
 </html>

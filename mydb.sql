06d11c1d0d2d21bf832e7f46ff948c0d1def19b5
b4stYgp <82322390+b4stYgp@users.noreply.github.com>
2022-06-22 11:59:48 +0200

    Create mydb.sql
    added mydb.sql for db creation

---
+++ mydb.sql
@@ -0,0 +1,119 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Erstellungszeit: 22. Jun 2022 um 11:58
+-- Server-Version: 10.4.24-MariaDB
+-- PHP-Version: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Datenbank: `mydb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `links`
+--
+
+CREATE TABLE `links` (
+  `id` int(11) NOT NULL,
+  `link` varchar(65) NOT NULL,
+  `timestamp` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `words`
+--
+
+CREATE TABLE `words` (
+  `id` int(11) NOT NULL,
+  `words` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `words_links`
+--
+
+CREATE TABLE `words_links` (
+  `id` int(11) NOT NULL,
+  `id_words` int(11) NOT NULL,
+  `id_links` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indizes der exportierten Tabellen
+--
+
+--
+-- Indizes für die Tabelle `links`
+--
+ALTER TABLE `links`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indizes für die Tabelle `words`
+--
+ALTER TABLE `words`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indizes für die Tabelle `words_links`
+--
+ALTER TABLE `words_links`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_word` (`id_words`) USING BTREE,
+  ADD KEY `id_links` (`id_links`);
+
+--
+-- AUTO_INCREMENT für exportierte Tabellen
+--
+
+--
+-- AUTO_INCREMENT für Tabelle `links`
+--
+ALTER TABLE `links`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT für Tabelle `words`
+--
+ALTER TABLE `words`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT für Tabelle `words_links`
+--
+ALTER TABLE `words_links`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints der exportierten Tabellen
+--
+
+--
+-- Constraints der Tabelle `words_links`
+--
+ALTER TABLE `words_links`
+  ADD CONSTRAINT `link_constraint` FOREIGN KEY (`id_links`) REFERENCES `links` (`id`),
+  ADD CONSTRAINT `word_constraint` FOREIGN KEY (`id_words`) REFERENCES `words` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;

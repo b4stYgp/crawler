e4b1f9d050ec98eb66710f500597cf2a29ed4ba3
b4stYgp <82322390+b4stYgp@users.noreply.github.com>
2022-06-22 15:39:39 +0200

    Update submit.php
    added iteration,
    fixed $links not an array warning

--- submit.php
+++ submit.php
@@ -42,71 +42,76 @@ class Crawler {
 	}
 }
 
-function crawl ($URL){
+function crawl ($URL,$iteration){
 	# von Sebastian Eisele und Tobias Zillmann
-	$servername = 'localhost';
-	$dbname = 'mydb';
-	$dbusername = 'root';
-	$dbpasswort = '';
-	$dbserverdaten = "mysql:host=$servername;dbname=$dbname";
-
-	#Datenbankverbindung herstellen
-	$verbindung = mysqli_connect($servername, $dbusername, $dbpasswort);
-
-	if(!$verbindung)
+	if($iteration>5)
 	{
-		echo "keine Verbindung";
-		exit;
+		echo "ITERATION GROSSER 5"; 
 	}
-
-	$datenbank = mysqli_select_db($verbindung, $dbname);
-
-	if(!$datenbank)
+	else
 	{
-		echo "keine Verbindung zur Datenbank";
-		exit;
-	}	
-	
-	#Speichern des Links falls noch nicht vorhanden ansonsten update des timestamps
-	$sql = "SELECT * FROM `links` WHERE `link`='$URL'";	
-	
-	if(mysqli_num_rows(mysqli_query($verbindung, $sql))){
-		$sql="UPDATE `links` SET `timestamp`= NOW() WHERE `link`='$URL'";
-	}
-	else{
-		$sql="INSERT INTO `links` (`link`,`timestamp`) VALUES ('$URL',NOW())";
-	}	
-	
-	$sqlErgebnis = mysqli_query($verbindung, $sql);
-	
-	$crawl = new Crawler($URL);
-	$images = $crawl->get('images');
-	$links = $crawl->get('links');
-	#echo implode("|",$links);
-	foreach($links as $link){	
-		array_push($links, $link);
-		$sql = "SELECT * FROM `links` WHERE `link` = '$link'";
-		$sqlErgebnis = mysqli_query($verbindung, $sql);
-		$reihen = mysqli_num_rows($sqlErgebnis);
-		echo "reihen1: $reihen";
-		if(!$reihen){
-			$sql = "INSERT INTO `links` (`link`) VALUES ('$link')";
-			$sqlErgebnis = mysqli_query($verbindung, $sql);
-		}
-		echo "<br>Link: $link";
-		$sql = "SELECT * FROM `links` WHERE `link` = '$link' AND (`timestamp` <  (NOW() - 86400) OR `timestamp` is NULL)";
-		echo $sql;
-		$sqlErgebnis = mysqli_query($verbindung, $sql);
-		if(mysqli_num_rows($sqlErgebnis)){
-			echo "new crawl initiate";
-			crawl($link);
-		}
-		mysqli_free_result($sqlErgebnis);
-	}		
-	mysqli_close($verbindung);
+		$servername = 'localhost';
+		$dbname = 'mydb';
+		$dbusername = 'root';
+		$dbpasswort = '';
+		$dbserverdaten = "mysql:host=$servername;dbname=$dbname";
 
+		#Datenbankverbindung herstellen
+		$verbindung = mysqli_connect($servername, $dbusername, $dbpasswort);
+
+		if(!$verbindung)
+		{
+			echo "keine Verbindung";
+			exit;
+		}
+
+		$datenbank = mysqli_select_db($verbindung, $dbname);
+
+		if(!$datenbank)
+		{
+			echo "keine Verbindung zur Datenbank";
+			exit;
+		}	
+		mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+		#Speichern des Links falls noch nicht vorhanden ansonsten update des timestamps
+		$sql = "SELECT * FROM `links` WHERE `link`='$URL'";	
+		
+		if(mysqli_num_rows(mysqli_query($verbindung, $sql))){
+			$sql="UPDATE `links` SET `timestamp`= NOW() WHERE `link`='$URL'";
+		}
+		else{
+			$sql="INSERT INTO `links` (`link`,`timestamp`) VALUES ('$URL',NOW())";
+		}	
+		
+		$sqlErgebnis = mysqli_query($verbindung, $sql);
+		
+		$crawl = new Crawler($URL);
+		$images = $crawl->get('images');
+		$links = $crawl->get('links');
+		if($links != NULL)
+		{
+			foreach($links as $link){	
+				array_push($links, $link);
+				$sql = "SELECT * FROM `links` WHERE `link` = '$link'";
+				$sqlErgebnis = mysqli_query($verbindung, $sql);
+				$reihen = mysqli_num_rows($sqlErgebnis);
+				if(!$reihen){
+					$sql = "INSERT INTO `links` (`link`) VALUES ('$link')";
+					$sqlErgebnis = mysqli_query($verbindung, $sql);
+				}
+				echo "<br>Link: $link";
+				$sql = "SELECT * FROM `links` WHERE `link` = '$link' AND (`timestamp` <  (NOW() - 86400) OR `timestamp` is NULL)";
+				$sqlErgebnis = mysqli_query($verbindung, $sql);
+				if(mysqli_num_rows($sqlErgebnis)){
+					echo "new crawl initiate";
+					crawl($link,$iteration+1);
+				}
+				mysqli_free_result($sqlErgebnis);
+			}				
+		}		
+		mysqli_close($verbindung);
+	}
 }
-
 $servername = 'localhost';
 $dbname = 'mydb';
 $dbusername = 'root';
@@ -135,8 +140,8 @@ if(isset($_POST))
 	mysqli_real_escape_string($verbindung, $_POST["url"]);
 	mysqli_close($verbindung);
 	$link = "https://";
-	$link .= $_POST["url"];
-	crawl($link);
+	$link .= $_POST["url"];	
+	crawl($link,0);
 }
 else{
 	echo "error";
